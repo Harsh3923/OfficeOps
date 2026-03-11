cb7516326f44d511b18946293fec73615d164b30
Harsh <h16patel@torontomu.ca>
2026-03-11 17:12:38 +0000

    created App.jsx for the frontend

---
+++ client/src/App.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+import VerifyOtpPage from "./pages/VerifyOtpPage";
+import UnauthorizedPage from "./pages/UnauthorizedPage";
+import ProtectedRoute from "./components/ProtectedRoute";
+import RoleRoute from "./components/RoleRoute";
+import EmployeeDashboard from "./pages/employee/EmployeeDashboard";
+import HRDashboard from "./pages/hr/HRDashboard";
+import ITDashboard from "./pages/it/ITDashboard";
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignupPage />} />
+      <Route path="/verify-otp" element={<VerifyOtpPage />} />
+      <Route path="/unauthorized" element={<UnauthorizedPage />} />
+
+      <Route element={<ProtectedRoute />}>
+        <Route element={<RoleRoute allowedRoles={["EMPLOYEE"]} />}>
+          <Route path="/employee" element={<EmployeeDashboard />} />
+        </Route>
+
+        <Route element={<RoleRoute allowedRoles={["HR"]} />}>
+          <Route path="/hr" element={<HRDashboard />} />
+        </Route>
+
+        <Route element={<RoleRoute allowedRoles={["IT"]} />}>
+          <Route path="/it" element={<ITDashboard />} />
+        </Route>
+      </Route>
+
+      <Route path="*" element={<Navigate to="/login" replace />} />
+    </Routes>
+  );
+}
